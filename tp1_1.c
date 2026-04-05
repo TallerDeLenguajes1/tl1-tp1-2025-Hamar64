71856d71f5740ba08c9ce72cf44c0ad92b27f151
MateoBolanez <soyundeifalso@gmail.com>
2026-04-05 15:58:57 -0300

    Segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    printf("hola mundo");
+
+    int num = 2;
+    int * p = &num;
+    printf("Contenido del puntero: %d\n", *p);
+    printf("Dirección almacenada por el puntero: %p\n", p);
+    printf("Dirección de memoria de la variable: %p\n", &num);
+    printf("Dirección de memoria del puntero: %p\n", &p);
+    printf("Tamaño de la variable: %d bytes\n", sizeof(num));
+
     return 0;
 }
